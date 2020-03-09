5aca78fa036d582dbfaafa77b262c81f36fb9436
madsongarciabd <madsongarciabd@gmail.com>
2020-03-09 00:05:27 -0300

    Classe global com as principais funções utilizadas nas outras classes

--- Bet365.py
+++ Bet365.py
@@ -1,10 +1,10 @@
-import selenium as SL
+from selenium import webdriver
 
 class Rb_Bet365:
     def __init__(self):
         pass
 
     def busca_dados (self):
-        teste = SL
+        self.driver = webdriver.Chrome(chrome_options=chromeOptions, executable_path=f"{utils.caminho_local()}\\arqs\\chromedriver.exe")
 
         pass
