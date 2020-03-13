ce57ff69db83c630687706e439b7227da7e812fd
madsongarciabd <madsongarciabd@gmail.com>
2020-03-13 00:06:20 -0300

    Primeiro Acesso ao BET365, iniciado busca dos dados.

--- RPA/Bet365.py
+++ RPA/Bet365.py
@@ -6,8 +6,17 @@ class Rb_Bet365:
         pass
 
     def busca_dados (self):
-        self.driver = webdriver.Chrome(executable_path=f"{utils.caminho_local()}\\arqs\\chromedriver.exe")
-        self.driver.get('https://www.bet365.com/#/HO/')
+        self.driver = webdriver.Chrome(executable_path=f"{utils.caminho_local()}\\chromedriver.exe")
+        self.driver.get('https://www.bet365.com/#/AC/B1/C1/D56/E0/F2/J0/Q1/F^24/')
+        nomes_times_tot = self.driver.find_elements_by_class_name('gll-Market')
+        colunas_valores_apostas = self.driver.find_elements_by_class_name('ufm-MarketC5OddsSwitchNoHeightGrow')
+        for times, coluna in zip (nomes_times_tot, colunas_valores_apostas):
+
+            print(coluna.text)
+            print(times.text)
+
+
+
 
 
         pass
---
+++ RPA/BuscaValores.py
@@ -0,0 +1,28 @@
+import RPA.Bet365 as bet
+
+
+
+
+
+
+def BuscaRPA():
+    print('eu')
+    objbet = bet.Rb_Bet365()
+    objbet.busca_dados()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    BuscaRPA()
